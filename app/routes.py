cbec7ec70662733abd06be34d610911f6da2457b
sltries <sltries@umich.edu>
2020-04-19 14:22:20 -0400

    Flask transition part 2

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
 from app import app
 from flask import render_template
 
+
+import csv
+import os
+
+
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
@@ -9,3 +15,44 @@ def index():
 @app.route('/sponsor')
 def sponsor():
     return render_template('sponsor.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/team')
+def team():
+    print(os.getcwd())
+    with open('./app/static/data/team.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='\t')
+        line_count = 0
+
+        art = []
+        audio = []
+        programming = []
+        design = []
+
+        for row in csv_reader:
+            print(row)
+            if (row[2] == "art"):
+                art.append(row)
+            elif (row[2] == "audio"):
+                audio.append(row)
+            elif (row[2] == "programming"):
+                programming.append(row)
+            elif (row[2] == "design"):
+                design.append(row)
+            else:
+                print('none')
+
+
+    return render_template('team.html', art=art, audio=audio, prog=programming, design=design)
+
+@app.route('/about')
+def about():
+    return "About page"
+
+@app.route('/build')
+def build():
+    return render_template('build.html')
